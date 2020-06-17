e4c2afcd9ea1f59769d316c609689d5eca63a6a0
hage19 <hage19@163.com>
2020-06-17 12:32:46 +0800

    add master 1

--- src/main/java/com/atguigu/mr/wordcount/WordCountDriver.java
+++ src/main/java/com/atguigu/mr/wordcount/WordCountDriver.java
@@ -16,6 +16,8 @@ public class WordCountDriver {
 	
 	//nihao dev1
 	
+	//nihao master 1
+	
 	// 在windows下无法直接提交job到YARN，可以在Linux下开启一个eclipse提交！
 	// 使用hadoop jar jar包   主类名
 	/*public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
